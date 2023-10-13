f9d3badeefa5ac6e8fa3af47ac6c49cc12206cc5
weka2670 <145996077+weka2670@users.noreply.github.com>
2023-10-14 02:05:52 +0800

    Add files via upload

---
+++ ks.js
@@ -0,0 +1,17 @@
+﻿function funName(){
+            document.title = "工具下载地址发布页";
+        };
+		
+function funName2(){
+			 document.write("10-14：最新修复不显示血条" +
+			 "<br>工具由互联网收集：<br>有问题可以咨询Skype:&lt;live:.cid.3661f3493476b481&gt;</a></h4><h4 style='color: black'>如链接无法访问,请自行尝试其他网络环境");
+        };
+		
+function funName3(){
+			 //alert("10-14：最新修复不显示血条");
+        };
+		
+
+				
+		
+		
