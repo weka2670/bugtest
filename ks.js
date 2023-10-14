c76a8b1e9b6bb203f1b7edb6ae5c9b67cb7cc0fc
weka2670 <145996077+weka2670@users.noreply.github.com>
2023-10-15 00:15:05 +0800

    Update ks.js

--- ks.js
+++ ks.js
@@ -3,12 +3,12 @@
         };
 		
 function funName2(){
-			 document.write("10-14：最新修复不显示血条" +
+			 document.write("10-15：最新修复不显示血条,导致的卡顿问题" +
 			 "<br>工具由互联网收集：<br>有问题可以咨询Skype:&lt;live:.cid.3661f3493476b481&gt;</a></h4><h4 style='color: black'>如链接无法访问,请自行尝试其他网络环境");
         };
 		
 function funName3(){
-			 //alert("10-14：最新修复不显示血条");
+			 alert("10-15：最新修复不显示血条,导致的卡顿问题");
         };
 		
 
